606cf8975a1d7dd5341fb665b2695aa469fab874
drap dagenius <drapdagenius@draps-MacBook-Pro.local>
2024-08-03 22:48:29 +0300

    implement initial file and folder structure2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: "Dradriga"};
   return (
-    <div>Home</div>
+    <section className='home'>
+      {/* Primary part of the home page */}
+      <div className='home-content'>
+        <header className='home-headere'>
+          <HeaderBox 
+            type ="greeting"
+            title = "welcome"
+            user = {loggedIn?.firstName || "Guest"}
+            subtext = "Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts = {[]}
+            totalBanks = {1}
+            totalCurrentBalance = {1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
